eee0e10a5adf6139c7cf6a31cdcab44d5fd3f9a5
neodevelop <neodevelop@gmail.com>
2013-08-15 19:24:22 -0500

    coding #1 agregando estructuras de clase

---
+++ grails-app/domain/com/makingdevs/Perfil.groovy
@@ -0,0 +1,28 @@
+package com.makingdevs
+
+class Perfil {
+
+  String nombre
+  String apellidoPaterno
+  String apellidoMaterno
+  Date fechaDeNacimiento
+  Sexo sexo = Sexo.QUIMERA
+  String avatar = "http://www.nelson-rives.com/images/noUserPhoto.png"
+
+  static hasMany = [direccion: Direccion, telefonos: Telefono]
+
+  Date dateCreated
+  Date lastUpdated
+
+  static constraints = {
+    nombre size:1..50,blank:false
+    apellidoPaterno size:1..50,blank:false
+    apellidoMaterno size:1..50,blank:true,nullable:true
+    fechaDeNacimiento nullable:true
+    avatar url:true
+  }
+
+  String nombreCompleto() {
+    "$nombre $apellidoPaterno ${apellidoMaterno ?: ''}"
+  }
+}
