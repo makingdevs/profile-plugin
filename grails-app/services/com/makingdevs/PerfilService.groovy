5332a1a5640e0cbaa125fc514623e84ec84a197c
Jorge Acosta <jorge@makingdevs.com>
2013-11-28 13:12:41 -0600

    coding #9 se corrigio error

--- grails-app/services/com/makingdevs/PerfilService.groovy
+++ grails-app/services/com/makingdevs/PerfilService.groovy
@@ -4,6 +4,8 @@ import org.grails.s3.S3Asset
 
 class PerfilService {
 
+  def s3AssetService
+
   def registrar(Perfil perfil) {
     perfil.save()
   }
@@ -18,15 +20,15 @@ class PerfilService {
     Perfil.findById(perfilId)
   }
 
-  def subirImagenPerfil(Long perfilId, file){
-    S3Asset receipt =new S3Asset()
-    Perfil perfil=Perfil.get(perfilId)
-    def temp = s3AssetService.getNewTmpLocalFile(file.contentType)
+  def subirImagenPerfil(Long perfilId, file) {
+    S3Asset receipt = new S3Asset()
+    Perfil perfil = Perfil.get(perfilId)
+    def tmp = s3AssetService.getNewTmpLocalFile(file.contentType)
     file.transferTo(tmp)
     receipt.newFile(tmp)
-    receipt.mimeType=file.contentType
+    receipt.mimeType = file.contentType
     s3AssetService.put(receipt)
-    perfil.avatar=receipt
+    perfil.avatar = receipt
     perfil.save()
     perfil
   }
