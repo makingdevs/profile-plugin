34f6f973f14db57fc50a6ee21cf0163cfbd944f9
Jorge Acosta <jorge@makingdevs.com>
2013-11-28 12:40:11 -0600

    coding #9 se agrego metodo para subir imagen a s3 del avatar de perfil

--- grails-app/services/com/makingdevs/PerfilService.groovy
+++ grails-app/services/com/makingdevs/PerfilService.groovy
@@ -1,5 +1,7 @@
 package com.makingdevs
 
+import org.grails.s3.S3Asset
+
 class PerfilService {
 
   def registrar(Perfil perfil) {
@@ -14,6 +16,19 @@ class PerfilService {
 
   def obtenerPerfilDeUsuario(def perfilId) {
     Perfil.findById(perfilId)
+  }
+
+  def subirImagenPerfil(Long perfilId, file){
+    S3Asset receipt =new S3Asset()
+    Perfil perfil=Perfil.get(perfilId)
+    def temp = s3AssetService.getNewTmpLocalFile(file.contentType)
+    file.transferTo(tmp)
+    receipt.newFile(tmp)
+    receipt.mimeType=file.contentType
+    s3AssetService.put(receipt)
+    perfil.avatar=receipt
+    perfil.save()
+    perfil
   }
 
 }
