c8a31ce047243bd850f863f3ee53fe5ddd062b77
Gamaliel Jimenez García <egjimenezg@gmail.com>
2014-07-21 14:45:10 -0500

    coding #11 agregando propiedad para que el perfil sea privado por default

--- grails-app/domain/com/makingdevs/Perfil.groovy
+++ grails-app/domain/com/makingdevs/Perfil.groovy
@@ -10,6 +10,7 @@ class Perfil {
   Date fechaDeNacimiento
   Sexo sexo = Sexo.QUIMERA
   S3Asset avatar
+  Boolean publico = false
 
   static searchable = {
     root false
@@ -25,6 +26,7 @@ class Perfil {
     apellidoPaterno size:1..50,blank:false
     apellidoMaterno size:1..50,blank:true,nullable:true
     fechaDeNacimiento nullable:true
+    publico blank:false,nullable:false
     avatar nullable:true
   }
 
