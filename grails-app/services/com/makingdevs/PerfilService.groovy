42f90c80abde19c391e53cb71a754c6bce737e34
temocpalma <temopalmag@gmail.com>
2016-06-23 15:43:34 -0500

    #12 Cambiando método para subir archivo de avatar del perfil

--- grails-app/services/com/makingdevs/PerfilService.groovy
+++ grails-app/services/com/makingdevs/PerfilService.groovy
@@ -1,10 +1,14 @@
 package com.makingdevs
 
-import org.grails.s3.S3Asset
+import com.amazonaws.AmazonClientException
+import com.amazonaws.AmazonServiceException
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.model.CannedAccessControlList
+import com.amazonaws.services.s3.model.PutObjectRequest
 
 class PerfilService {
-
-  def s3AssetService
 
   def registrar(Perfil perfil) {
     perfil.save()
@@ -21,14 +25,19 @@ class PerfilService {
   }
 
   def subirImagenPerfil(Long perfilId, file) {
-    S3Asset receipt = new S3Asset()
     Perfil perfil = Perfil.get(perfilId)
-    def tmp = s3AssetService.getNewTmpLocalFile(file.contentType)
-    file.transferTo(tmp)
-    receipt.newFile(tmp)
-    receipt.mimeType = file.contentType
-    s3AssetService.put(receipt)
-    perfil.avatar = receipt
+
+    AmazonS3 s3client = new AmazonS3Client(new BasicAWSCredentials(accessKey, secretKey))
+    try {
+      s3client.putObject(new PutObjectRequest(bucket, file.name, file).withCannedAcl(CannedAccessControlList.PublicRead));
+    } catch (AmazonServiceException asEx){
+      throw new FacturaException(asEx.getMessage())
+    } catch (AmazonClientException acEx){
+      throw new FacturaException(acEx.getMessage())
+    }
+    url = "${bucket}.${urlS3}/${file.name}"
+
+    perfil.avatar = url
     perfil.save()
     perfil
   }
