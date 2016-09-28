919aee437751c6235e7276c884d8bd4cb4e13f42
carlogilmar <carlogilmar12@gmail.com>
2016-09-28 11:35:59 -0500

    Add import, extends, modify grailsV,def profile, and add methods

--- src/main/groovy/grails/plugins/ProfileGrailsPlugin.groovy
+++ src/main/groovy/grails/plugins/ProfileGrailsPlugin.groovy
@@ -1,18 +1,22 @@
 package grails.plugins.profile
 
-class ProfileGrailsPlugin {
+import grails.plugins.*
+
+class ProfileGrailsPlugin extends Plugin {
   def groupId = "com.makingdevs"
-  def grailsVersion = "2.0 > *"
+  def grailsVersion = "3.1.8 > *"
   //def dependsOn = ["spring-security-core":"1.2.7.0 > 1.2.7.4" ]
-  /*def pluginExcludes = [
+  def pluginExcludes = [
     "grails-app/views/error.gsp"
-  ]*/
+  ]
 
   def author = "MakingDevs"
   def authorEmail = "info@makingdevs.com"
   /*def description = '''\
   This plugin is intended to externalize behavior and features to manage the profile for each user using Spring Security
   '''*/
+  def profiles = ['web']
+
   def documentation = "https://github.com/makingdevs/profile-plugin"
   def license = "APACHE"
   def organization = [ name: "MakingDevs", url: "http://makingdevs.com/" ]
@@ -27,17 +31,32 @@ class ProfileGrailsPlugin {
 
   def scm = [ url: "https://github.com/makingdevs/profile-plugin" ]
 
-  def doWithWebDescriptor = { xml -> }
+  Closure doWithSpring() { {->
+    // TODO Implement runtime spring config (optional)
+  }
+  }
 
-  def doWithSpring = { }
+  void doWithDynamicMethods() {
+    // TODO Implement registering dynamic methods to classes (optional)
+  }
 
-  def doWithDynamicMethods = { ctx -> }
+  void doWithApplicationContext() {
+    // TODO Implement post initialization spring config (optional)
+  }
 
-  def doWithApplicationContext = { applicationContext -> }
+  void onChange(Map<String, Object> event) {
+    // TODO Implement code that is executed when any artefact that this plugin is
+    // watching is modified and reloaded. The event contains: event.source,
+    // event.application, event.manager, event.ctx, and event.plugin.
+  }
 
-  def onChange = { event -> }
+  void onConfigChange(Map<String, Object> event) {
+    // TODO Implement code that is executed when the project configuration changes.
+    // The event is the same as for 'onChange'.
+  }
 
-  def onConfigChange = { event -> }
+  void onShutdown(Map<String, Object> event) {
+    // TODO Implement code that is executed when the application shuts down (optional)
+  }
 
-  def onShutdown = { event -> }
 }
